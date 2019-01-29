81f330138b73bbb84342c22a07c65655b88a50ce
Danil Guskov <d.guskov@corp.mail.ru>
2019-01-29 15:40:21 +0300

    clap skeleton init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate clap;
+
+use clap::{Arg, App, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("rb-proxy")
+        .version("0.0.1")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::with_name("address")
+             .long("address")
+             .default_value("0.0.0.0")
+             .help("The address to bind to")
+        )
+        .arg(Arg::with_name("port")
+             .long("port")
+             .default_value("8080")
+             .help("The port to listen on")
+        )
+        .arg(Arg::with_name("proxy_port_range")
+             .long("proxyPortRange")
+             .default_value("8081..8581")
+             .help("The range of ports to use for proxies")
+        )
+        .arg(Arg::with_name("ttl")
+             .long("ttl")
+             .default_value("0")
+             .help("Time in seconds until an unused proxy")
+        )
+        .get_matches();
 }
