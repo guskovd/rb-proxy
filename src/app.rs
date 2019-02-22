b2c06ece72be2033ea927ead20d025afb96f46f7
Danil Guskov <d.guskov@corp.mail.ru>
2019-02-22 18:15:51 +0300

    refactor tests

--- src/app.rs
+++ src/app.rs
@@ -21,25 +21,40 @@ mod tests {
     use tokio::runtime::current_thread::Runtime;
     use std::thread;
 
+    const PORT: u16 = 8080;
+
     #[test]
     fn smoke_test() {
-        
-        let port = 8080;
+        serve();
+        request("/");
+    }
+
+    #[test]
+    fn fake_test() {
+        serve();
+        request("/hello");
+    }
+
+    fn serve() {
         let _thread = thread::Builder::new().name(String::from("thread")).spawn(move || {
             let (_tx, rx) = oneshot::channel();
             let (_addr, fut) = warp::serve(super::routes())
-                .bind_with_graceful_shutdown(([127, 0, 0, 1], port), rx);
+                .bind_with_graceful_shutdown(([127, 0, 0, 1], PORT), rx);
             
             let mut rt = Runtime::new().expect("rt new");
 
             rt.block_on(fut).unwrap();
         }).expect("thread spawn");
 
+    }
+
+    fn request(path: &str) {
         let mut rt = Runtime::new().expect("rt new");
-        let addr_str = format!("http://127.0.0.1:{}", port);
+        let addr_str = format!("http://127.0.0.1:{}/{}", PORT, path);
         rt.block_on(hyper::rt::lazy(move || {
             let client = Client::new();
             let uri = addr_str.parse().expect("server addr should parse");
+            println!("{:?}", uri);
             client.get(uri)
         })).unwrap();
     }
