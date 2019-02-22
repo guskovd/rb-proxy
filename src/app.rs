19bd4ddd1f9bc702f0663a0d4d7160b9ff013b49
Danil Guskov <d.guskov@corp.mail.ru>
2019-02-22 19:25:37 +0300

    randomize test ports

--- src/app.rs
+++ src/app.rs
@@ -21,39 +21,44 @@ pub fn app(port: u16) {
 mod tests {
     use hyper::client::Client;
     use futures::sync::oneshot;
+    use std::sync::mpsc;
     use tokio::runtime::current_thread::Runtime;
     use std::thread;
 
-    const PORT: u16 = 8080;
-
     #[test]
     fn smoke_test() {
-        serve();
-        request("/");
+        let port = serve();
+        request("/", port);
     }
 
     #[test]
     fn fake_test() {
-        serve();
-        request("/hello");
+        let port = serve();
+        request("/", port);
     }
 
-    fn serve() {
+    fn serve() -> u16 {
+        let (port_tx, port_rx) = mpsc::channel();
         let _thread = thread::Builder::new().name(String::from("thread")).spawn(move || {
             let (_tx, rx) = oneshot::channel();
-            let (_addr, fut) = warp::serve(super::routes())
-                .bind_with_graceful_shutdown(([127, 0, 0, 1], PORT), rx);
+            let (addr, fut) = warp::serve(super::routes())
+                .bind_with_graceful_shutdown(([127, 0, 0, 1], 0), rx);
+
+            port_tx.send(
+                addr.port()
+            ).expect("server addr tx");
             
             let mut rt = Runtime::new().expect("rt new");
 
             rt.block_on(fut).unwrap();
         }).expect("thread spawn");
-
+        let port = port_rx.recv().expect("server addr rx");
+        port
     }
 
-    fn request(path: &str) {
+    fn request(path: &str, port: u16) {
         let mut rt = Runtime::new().expect("rt new");
-        let addr_str = format!("http://127.0.0.1:{}{}", PORT, path);
+        let addr_str = format!("http://127.0.0.1:{}{}", port, path);
         rt.block_on(hyper::rt::lazy(move || {
             let client = Client::new();
             let uri = addr_str.parse().expect("server addr should parse");
