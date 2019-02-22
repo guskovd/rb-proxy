7c1701ee9bca380cad979aeaa4069b93cc6a6592
Danil Guskov <d.guskov@corp.mail.ru>
2019-02-22 18:22:06 +0300

    refactor tests

--- src/app.rs
+++ src/app.rs
@@ -4,8 +4,11 @@ extern crate futures;
 use warp::{Filter,Rejection};
 
 pub fn routes() -> impl Filter<Extract = (String,), Error = Rejection> {
-    let server = path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let server = warp::path::path("hello")
+        .and(warp::path::param())
+        .map(|name: String| {
+            format!("Hello, {}!", name)
+        });
     server
 }
 
@@ -50,7 +53,7 @@ mod tests {
 
     fn request(path: &str) {
         let mut rt = Runtime::new().expect("rt new");
-        let addr_str = format!("http://127.0.0.1:{}/{}", PORT, path);
+        let addr_str = format!("http://127.0.0.1:{}{}", PORT, path);
         rt.block_on(hyper::rt::lazy(move || {
             let client = Client::new();
             let uri = addr_str.parse().expect("server addr should parse");
