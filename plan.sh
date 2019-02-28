31926ff45bd157a335f018339636a3b2764568b9
Danil Guskov <d.guskov@corp.mail.ru>
2019-02-28 20:15:16 +0300

    new rust setup

--- plan.sh
+++ plan.sh
@@ -35,23 +35,37 @@ pkg_deps=(
 )
 
 do_shell() {
-    export RUST_SRC_PATH=$HOME/rust/src/
-    export CARGO_HOME=$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.cargo
+    local_rust
+    
     export PKG_CONFIG_PATH="$(hab pkg path core/libsodium)/lib/pkgconfig:$(hab pkg path core/libarchive)/lib/pkgconfig:$(hab pkg path core/openssl)/lib/pkgconfig"
-
-    ruby_bundle_path=$HOME/.hab-shell/ruby/bundle/$RUBY_VERSION
-
-    mkdir -p $ruby_bundle_path
-    export BUNDLE_PATH=$ruby_bundle_path
-
-    pushd "$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" > /dev/null
-    bundle install --binstubs > /dev/null
-    popd > /dev/null
+    export BUNDLE_PATH=$HOME/.hab-shell/ruby/bundle/$RUBY_VERSION
 
     export PATH="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.hab-shell/bin:$PATH"
     export PATH="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bin:$PATH"
     
     . ~/.bashrc
+}
+
+ruby_setup () {
+    ruby_bundle_path=$HOME/.hab-shell/ruby/bundle/$RUBY_VERSION
+    mkdir -p $ruby_bundle_path
+
+    pushd "$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" > /dev/null
+    bundle install --binstubs
+    popd > /dev/null
+}
+
+local_rust() {
+    export CARGO_HOME=$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.cargo
+    
+    plan_path="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    mkdir -p $plan_path/.rust
+    commit=$(rustc -v --version | grep commit-hash | awk '{print $2}')
+    if [[ ! -d $plan_path/.rust/rust-$commit ]]; then
+	wget https://github.com/rust-lang/rust/archive/${commit}.zip -O /tmp/${commit}.zip
+	unzip -qq /tmp/${commit}.zip -d $plan_path/.rust
+    fi
+    export RUST_SRC_PATH=$plan_path/.rust/rust-$commit/src/
 }
 
 do_build() {
